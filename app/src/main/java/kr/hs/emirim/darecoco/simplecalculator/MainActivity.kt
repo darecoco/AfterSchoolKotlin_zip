7ec3151f110bc974638bc41af2782fcf96f6135b
darecoco <whaqlrh37@naver.com>
2023-05-16 19:11:50 +0900

    when으로 listener사용

--- app/src/main/java/kr/hs/emirim/darecoco/simplecalculator/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/darecoco/simplecalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.hs.emirim.darecoco.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -31,59 +33,31 @@ class MainActivity : AppCompatActivity() {
         btnDivide = findViewById(R.id.btn_divide)
         btnMod = findViewById(R.id.btn_mod)
 
-        btnAdd.setOnClickListener {
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            if(num1=="" || num2==""){
-                Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            result = num1.toDouble() + num2.toDouble()
-            textResult.text = "계산 결과 : " + result
+        btnAdd.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultipy.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+    }
+
+    var btnListener = OnClickListener{
+        num1 = edit1.text.toString()
+        num2 = edit2.text.toString()
+        if(num1=="" || num2==""){
+            Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
+            return@OnClickListener
         }
-        btnMinus.setOnClickListener {
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            if(num1=="" || num2==""){
-                Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+        when(it.id){
+            R.id.btn_plus -> result = num1.toDouble() + num2.toDouble()
+            R.id.btn_minus -> result = num1.toDouble() - num2.toDouble()
+            R.id.btn_multiply -> result = num1.toDouble() * num2.toDouble()
+            R.id.btn_divide -> {
+                if(num2 == "0"){
+                    Toast.makeText(baseContext, "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    return@OnClickListener
+                }
+                result = num1.toDouble() / num2.toDouble()
             }
-            result = num1.toDouble() + num2.toDouble()
-            textResult.text = "계산 결과 : " + result
         }
-        btnMultipy.setOnClickListener {
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            if(num1=="" || num2==""){
-                Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            result = num1.toDouble() - num2.toDouble()
-            textResult.text = "계산 결과 : " + result
-        }
-        btnDivide.setOnClickListener {
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            if(num1=="" || num2==""){
-                Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            result = num1.toDouble() / num2.toDouble()
-            textResult.text = "계산 결과 : " + result
-        }
-        btnMod.setOnClickListener {
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            if(num1=="" || num2==""){
-                Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            if(num2 == "0"){
-                Toast.makeText(baseContext, "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            result = num1.toDouble() % num2.toDouble()
-            textResult.text = "계산 결과 : " + result
-        }
+        textResult.text = "계산 결과 : " + result
     }
 }
