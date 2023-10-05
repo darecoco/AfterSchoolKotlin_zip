b97423495feb9cf522f65c7c2c94a4abb4b73d67
darecoco <whaqlrh37@naver.com>
2023-10-05 20:08:20 +0900

    Main, Result레이아수 작성

---
+++ app/src/main/java/kr/hs/emirim/darecoco/vote_10_2/ReasultActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.darecoco.vote_10_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReasultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reasult)
+    }
+}
